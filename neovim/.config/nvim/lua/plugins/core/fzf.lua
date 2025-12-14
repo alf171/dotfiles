fe14c3500e819fb287580252e67c13e0b60831c4
alf171 <alfonso.laffont@gmail.com>
2025-12-14 00:12:05 -0500

    revert breaking previewer in fzf

--- neovim/.config/nvim/lua/plugins/core/fzf.lua
+++ neovim/.config/nvim/lua/plugins/core/fzf.lua
@@ -1,33 +1,34 @@
 return {
   'ibhagwan/fzf-lua',
   dependencies = { 'nvim-mini/mini.icons' },
-  config = function()
+  config = function(_, opts)
     local fzf = require 'fzf-lua'
-    local km = vim.keymap
+    local map = vim.keymap.set
+    fzf.setup(opts)
 
-    km.set('n', '<leader>f', function()
+    map('n', '<leader>f', function()
       fzf.files {
-        previewer = { enabled = false },
+        previewer = false,
       }
     end, { desc = 'search for file with exact name' })
 
-    km.set('n', '<leader>sf', function()
+    map('n', '<leader>sf', function()
       fzf.files()
     end, { desc = 'fuzzy search for files with preview' })
 
-    km.set('n', '<leader>sg', function()
+    map('n', '<leader>sg', function()
       fzf.live_grep()
     end, { desc = 'grep for contents of a file' })
 
-    km.set('n', '<leader>o', function()
+    map('n', '<leader>o', function()
       fzf.buffers {
-        previewer = { enabled = false },
+        previewer = false,
       }
     end, { desc = 'list open buffers' })
 
-    km.set('n', '<leader>ca', function()
+    map('n', '<leader>ca', function()
       fzf.lsp_code_actions {
-        previewer = { enabled = false },
+        previewer = false,
         silent = true,
       }
     end, { desc = 'suggest a code action' })
