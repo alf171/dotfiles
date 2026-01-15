cedf7bd86f4d5cd12700db1cfba48bc9e7554770
alf171 <alfonso.laffont@gmail.com>
2026-01-15 00:32:03 -0500

    add quill changes

--- neovim/.config/nvim/lua/plugins/handmade/quill.lua
+++ neovim/.config/nvim/lua/plugins/handmade/quill.lua
@@ -1,15 +1,16 @@
 return {
-  'alf171/quill.nvim',
-  -- dir = '~/Desktop/projects/nvim-plugins/quill.nvim/',
+  -- 'alf171/quill.nvim',
+  dir = '~/Desktop/projects/nvim-plugins/quill.nvim/',
   config = function()
     require('quill').setup {
-      notes_path = '~/Desktop/notes',
+      notes_path = '~/Desktop/notes/',
       keymaps = {
         open = '<leader>td',
         close = 'q',
         prev = '<C-p>',
         next = '<C-n>',
       },
+      debug = true,
     }
   end,
 }
