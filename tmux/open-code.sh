f45dc0333527703c0b71580ecbfc4c891af346cc
alf171 <alfonso.laffont@gmail.com>
2026-02-17 15:50:37 -0500

    tmux: Implement SSH connection handling for URL opening
    Add SSH handling for opening URLs in open-code.sh

--- tmux/open-code.sh
+++ tmux/open-code.sh
@@ -9,15 +9,17 @@ url=$(git remote get-url origin 2>/dev/null || echo "")
 if [[ $url == *"github.com"* ]]; then
   clean_url=${url%.git}
   clean_url=${clean_url/git@github.com:/https://github.com/}
-  open "$clean_url"
+  target_url="$clean_url"
 # open on c.a
 else
   repo=$(basename -s .git "$url")
   repo=${repo:-$(basename "$PWD")}
-  custom_url="https://code.amazon.com/packages/${repo}/trees/mainline"
-  # This done not work over ssh. For that try:
-  # ssh -M -S ~/.ssh/tmux-socket remote-host
-  # ssh -S ~/.ssh/tmux-socket -O forward localhost "open '$clean_url'"
-  open "$custom_url"
+  target_url="https://code.amazon.com/packages/${repo}/trees/mainline"
 fi
 
+# over ssh, print the target since open won't work
+if [[ -n "$SSH_CONNECTION" ]]; then
+    echo "$target_url"
+else
+    open "$target_url"
+fi
