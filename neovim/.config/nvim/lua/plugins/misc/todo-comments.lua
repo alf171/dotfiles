279ea748f892dc89e1dbe51f194f777766b582f4
alf171 <alfonso.laffont@gmail.com>
2026-04-22 23:33:42 -0400

    feat: commit package branches and remove unused package

--- neovim/.config/nvim/lua/plugins/misc/todo-comments.lua
+++ neovim/.config/nvim/lua/plugins/misc/todo-comments.lua
@@ -1,11 +1,6 @@
 return {
   pack = {
-    {
-      src = 'https://github.com/nvim-lua/plenary.nvim',
-    },
-    {
-      src = 'https://github.com/folke/todo-comments.nvim',
-    },
+    src = 'https://github.com/folke/todo-comments.nvim',
   },
   setup = function()
     require('todo-comments').setup()
