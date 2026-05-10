4b18e5503303a68bb82c620e46737f711d6a5b30
alf171 <alfonso.laffont@gmail.com>
2026-05-10 11:15:40 -0400

    run zig format on save

--- neovim/.config/nvim/ftplugin/zig.lua
+++ neovim/.config/nvim/ftplugin/zig.lua
@@ -2,3 +2,16 @@ vim.opt_local.tabstop = 2
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.expandtab = true
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  buffer = 0,
+  callback = function()
+    vim.system({ 'zig', 'fmt', vim.api.nvim_buf_get_name(0) }, {}, function()
+      vim.schedule(function()
+        if vim.api.nvim_buf_is_loaded(0) then
+          vim.cmd.checktime()
+        end
+      end)
+    end)
+  end,
+})
